adbc5b4bc55ae562839f73eacf9d6e6854cc9ca6
Evgeny Ivanov <evg226@hotmail.com>
2022-08-19 15:06:42 +0500

    Made production

---
+++ build/precache-manifest.b5cd9f18f3c810d9fad01c81d3b2b35c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "bd208108bdb1802ac693",
+    "url": "/static/css/main.bc182ccc.chunk.css"
+  },
+  {
+    "revision": "bd208108bdb1802ac693",
+    "url": "/static/js/main.bd208108.chunk.js"
+  },
+  {
+    "revision": "407340cd97cf875d09d1",
+    "url": "/static/js/1.407340cd.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "1655766bd11bac5fe58052c233d7fc78",
+    "url": "/index.html"
+  }
+];
